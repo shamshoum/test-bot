bafdf876a0ec95aaea2761ff8204d3a45e8e9c3a
Yousef Shamshoum <shamshoumyousef@gmail.com>
2017-02-10 15:40:19 +0200

    Updated the bot to speak

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const app = express()
+const app = express();
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -30,3 +30,37 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+const token = "EAAaD5sroCZCsBAKnSZCW6cKGnSORLs4i5FAo61rRyBXiLqVmPo3Ud7dabhHO6Dawv5baeZC8NarVUvV7gs4MAx32uDh7NcoPaoOrhqciOzZCZA9tBzyl6SdourtRB6HhNLpz67UIzJdR2jpPHf0ZBnyJXCVFFWvCnYxaZCZBCF94HAZDZD"
