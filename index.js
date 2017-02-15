ff8f9cc7de95e791e9daf8b964e134ae868c16d0
Yousef Shamshoum <shamshoumyousef@gmail.com>
2017-02-15 04:11:08 +0200

    no message

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/webhook/', function (req, res) {
     // Checking if user exists in db
     let req = {
       url: 'http://bot-parse.herokuapp.com/parse/classes/facebookuser',
-      headers: {f
+      headers: {fdfasdf
         'Content-Type': 'application/json',
         'X-Parse-Application-Id': 'botparse1967',
         'X-Parse-Master-Key': 'botparsemasterkey'
